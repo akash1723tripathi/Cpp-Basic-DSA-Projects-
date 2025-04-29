556079e2c2f2917e6bc0d86256c1ebaba40c10f5
akash1723tripathi <akashtripathi.acad@gmail.com>
2025-04-30 01:42:34 +0530

    new change

--- 1_Basics/Basic Math/5_AllDivisors.cpp
+++ 1_Basics/Basic Math/5_AllDivisors.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
 int* printDivisors(int n, int &size) {
     int* temp = new int[n]; // temporary array to hold divisors
     size = 0;
 
-    for(int i = 1; i <= n; i++) {
-        if(n % i == 0) {
-            temp[size++] = i; // store divisor and increment size
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            temp[size++] = i;
+            if (n / i != i) {
+                temp[size++] = n / i;
+            }
         }
     }
 
+    // Sort the array
+    sort(temp, temp + size);
+
     // Create exact-sized array to return
     int* result = new int[size];
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         result[i] = temp[i];
     }
 
